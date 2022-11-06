66f4342597f8a89131a6f4473b02ce7f88e707bb
Laura Berge <lauraeberge@gmail.com>
2022-11-05 21:58:01 -0500

    add solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self._word = word
+
+    def match(self, list):
+        return [word for word in list if set(word) == set(self._word)]
